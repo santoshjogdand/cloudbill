06d9f67ecd429b49a04b39f8911182cb44366e62
Rohitthorbole <rohitthorbole4@gmail.com>
2025-01-13 19:44:15 +0530

    Sales

--- src/components/Sales/Sales.jsx
+++ src/components/Sales/Sales.jsx
@@ -3,17 +3,25 @@ import React from 'react'
 const Sales = () => {
   return (
     <div className='main h-full m-0 p-0'>
-      <div className='left h-full w-[50vw] bg-blue-200'>
-        <div className='customer_details h-[25vh] w-full'>
-          <div className="title">CUSTOMER DETAILS</div>
-          <div className="customer_details_box"></div>
+      <div className='left h-full w-full bg-blue-200'>
+        <div className='customer_details p-2 h-[25vh] w-full'>
+          <div className="title ">CUSTOMER DETAILS</div>
+          <div className="customer_details_box">
+            <label htmlFor="">Name</label>
+            <input type="text" id="cname" />
+            <label htmlFor="">Phone</label>
+            <input type="text" id="cphone" />
+            <label htmlFor="">Address</label>
+            <input type="text" id="caddress"/>
+            <label htmlFor="">Email</label>
+            <input type="text" id="cemail"/>
+          </div>
         </div>
-        <div className='product_details'>
+        <div className='product_details p-2'>
           <div className="title">PRODUCT DETAILS</div>
           <div className="product_details_box"></div>
         </div>
       </div>
-      <div className='right'></div>
     </div>
   )
 }
