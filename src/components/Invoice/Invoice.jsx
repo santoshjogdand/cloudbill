6853c3906b612c4e27c750127d797b19344df91e
Rohitthorbole <rohitthorbole4@gmail.com>
2025-02-06 14:59:36 +0530

    Customer done

--- src/components/Invoice/Invoice.jsx
+++ src/components/Invoice/Invoice.jsx
@@ -17,75 +17,94 @@ const Invoice = () => {
   return (
     <div className='main w-full h-full bg-blue-100 flex'>
       <div className='left w-[55vw] h-full'>
-        <div className='customer_info flex justify-between mt-10 px-10'>
-          <div className='customer_info_left h-[20vh] w-[23vw] bg-white rounded-lg'>
-            <div className='c_name_contact flex justify-between mt-2 text-sm px-4'>
-              <div className='cname'>
-                <div>INVOICE NO</div>
-                <div>12356</div>
+        <div className='customer'>
+          <div className='title flex justify-between mt-5 px-10'>
+            <div>CUSTOMER DETAILS</div>
+            <div className="customer_number relative w-full max-w-xs">
+              <img
+                src="../src/assets/search.svg"
+                alt="search"
+                className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400"
+              />
+              <input
+                type="text"
+                placeholder="Customer Name"
+                className="pl-10 p-2 border border-gray-300 rounded w-full outline-none text-sm text-gray-700 focus:ring-1 focus:ring-blue-400"
+              />
+            </div>
+          </div>
+          <div className='customer_info flex justify-between mt-10 px-10'>
+            <div className='customer_info_left h-[20vh] w-[23vw] bg-white rounded-lg'>
+              <div className='c_name_contact flex justify-between mt-2 text-sm px-4'>
+                <div className='cname'>
+                  <div>INVOICE NO</div>
+                  <div>12356</div>
+                </div>
+                <div className='ccontact'>
+                  <div>CONTACT NO</div>
+                  <div>
+                    <input
+                      type="tel"
+                      placeholder="Enter mobile number"
+                      pattern="[0-9]{10}"
+                      maxLength="10"
+                      ref={mobileNumberRef}
+                      className="border-none outline-none bg-transparent"
+                    />
+                  </div>
+                </div>
               </div>
-              <div className='ccontact'>
-                <div>CONTACT NO</div>
+              <div className='separating_line border-t border-gray-300 my-2'></div>
+              <div className='c_address px-4 text-sm'>
+                <div className='c_address_title'>DATE</div>
                 <div>
-                  <input
-                    type="tel"
-                    placeholder="Enter mobile number"
-                    pattern="[0-9]{10}"
-                    maxLength="10"
-                    ref={mobileNumberRef}
-                    className="border-none outline-none bg-transparent"
-                  />
+                  <input type="date" ref={dateRef} className="border-none outline-none bg-transparent" />
                 </div>
               </div>
             </div>
-            <div className='separating_line border-t border-gray-300 my-2'></div>
-            <div className='c_address px-4 text-sm'>
-              <div className='c_address_title'>DATE</div>
-              <div>
-                <input type="date" ref={dateRef} className="border-none outline-none bg-transparent" />
-              </div>
-            </div>
-          </div>
 
-          <div className='customer_info_right h-[20vh] w-[23vw] bg-white rounded-lg'>
-            <div className='c_name_contact flex justify-between mt-2 text-sm mx-4'>
-              <div className='cname'>
-                <div>CUSTOMER NAME</div>
+            <div className='customer_info_right h-[20vh] w-[23vw] bg-white rounded-lg'>
+              <div className='c_name_contact flex justify-between mt-2 text-sm mx-4'>
+                <div className='cname'>
+                  <div>CUSTOMER NAME</div>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Enter customer name"
+                      maxLength="50"
+                      ref={customerNameRef}
+                      className="border-none outline-none bg-transparent"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <img
+                    src='../src/assets/delete.svg'
+                    alt="Delete"
+                    onClick={clearInputs}
+                    className="cursor-pointer w-5 h-5"
+                  />
+                </div>
+              </div>
+              <div className='separating_line border-t border-gray-300 my-2'></div>
+              <div className='c_address px-4 text-sm'>
+                <div className='c_address_title'>ADDRESS</div>
                 <div>
                   <input
                     type="text"
-                    placeholder="Enter customer name"
+                    placeholder="Enter address"
                     maxLength="50"
-                    ref={customerNameRef}
+                    ref={addressRef}
                     className="border-none outline-none bg-transparent"
                   />
                 </div>
-              </div>
-              <div>
-                <img
-                  src='../src/assets/delete.svg'
-                  alt="Delete"
-                  onClick={clearInputs}
-                  className="cursor-pointer w-5 h-5"
-                />
-              </div>
-            </div>
-            <div className='separating_line border-t border-gray-300 my-2'></div>
-            <div className='c_address px-4 text-sm'>
-              <div className='c_address_title'>ADDRESS</div>
-              <div>
-                <input
-                  type="text"
-                  placeholder="Enter address"
-                  maxLength="50"
-                  ref={addressRef}
-                  className="border-none outline-none bg-transparent"
-                />
               </div>
             </div>
           </div>
         </div>
+
         <div className='separating_line border-t border-gray-400 my-3'></div>
+
         <div className='Product_details'>
           <div className='title flex justify-between px-10'>
             <div className='ptitle text-base'>PRODUCT CART</div>
@@ -120,6 +139,7 @@ const Invoice = () => {
           </div>
         </div>
       </div>
+      
       <div className='right w-[31vw] h-full bg-gray-50'>
         <div className='detail_payment mt-5 px-5'>
           <div className="title text-xl my-3">Invoice</div>
