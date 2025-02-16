b75ba4f47a7aa427c02ee411e7adf5ab780a1e56
Rohitthorbole <rohitthorbole4@gmail.com>
2025-02-16 21:11:14 +0530

    Sales 75

--- src/components/Sales/Sales.jsx
+++ src/components/Sales/Sales.jsx
@@ -16,15 +16,15 @@ const Sales = () => {
   };
 
   return (
-    <div className='main bg-blue-100 w-full h-full pl-[30vh]'>
+    <div className='main bg-blue-100 w-full h-fit pl-[30vh] pb-[100vh]'>
       {/* Title */}
-      <div className='title text-xl flex justify-center py-5 font-bold'>Sales</div>
+      <div className='title text-[1.5vw] flex justify-center py-5 font-bold'>Sales</div>
 
       {/* Filter Section */}
       <div className='filters flex justify-around items-center mb-5'>
         {/* Amount Filter */}
         <div className='filter-option'>
-          <label htmlFor='amount' className='block text-sm font-medium mb-1'>Amount</label>
+          <label htmlFor='amount' className='block text-[2.5vh] font-medium mb-1'>Amount</label>
           <select
             id='amount'
             className='p-1 border border-gray-300 rounded w-36 text-sm'
@@ -39,10 +39,10 @@ const Sales = () => {
 
         {/* Date Filter */}
         <div className='filter-option'>
-          <label htmlFor='date' className='block text-sm font-medium mb-1'>Date</label>
+          <label htmlFor='date' className='block text-[2.5vh] font-medium mb-1'>Date</label>
           <select
             id='date'
-            className='p-1 border border-gray-300 rounded w-36 text-sm'
+            className='p-1 border border-gray-300 rounded w-36 text-[2vh]'
             value={filters.date}
             onChange={(e) => handleFilterChange('date', e.target.value)}
           >
@@ -54,11 +54,11 @@ const Sales = () => {
 
         {/* From Date Filter */}
         <div className='filter-option'>
-          <label htmlFor='fromDate' className='block text-sm font-medium mb-1'>From Date</label>
+          <label htmlFor='fromDate' className='block text-[2.5vh] font-medium mb-1'>From Date</label>
           <input
             type='date'
             id='fromDate'
-            className='p-1 border border-gray-300 rounded w-36 text-sm'
+            className='p-1 border border-gray-300 rounded w-36 text-[2vh]'
             value={filters.fromDate}
             onChange={(e) => handleFilterChange('fromDate', e.target.value)}
           />
@@ -66,11 +66,11 @@ const Sales = () => {
 
         {/* To Date Filter */}
         <div className='filter-option'>
-          <label htmlFor='toDate' className='block text-sm font-medium mb-1'>To Date</label>
+          <label htmlFor='toDate' className='block text-[2.5vh] font-medium mb-1'>To Date</label>
           <input
             type='date'
             id='toDate'
-            className='p-1 border border-gray-300 rounded w-36 text-sm'
+            className='p-1 border border-gray-300 rounded w-36 text-[2vh]'
             value={filters.toDate}
             onChange={(e) => handleFilterChange('toDate', e.target.value)}
           />
@@ -83,7 +83,7 @@ const Sales = () => {
         <div className='columns mt-10'>
           <table className="table-auto w-[80%] mx-auto border-collapse border border-gray-300">
             <thead>
-              <tr className="bg-blue-600 text-white">
+              <tr className="bg-blue-600 text-white text-[2.5vh]">
                 <th className="border border-gray-400 px-4 py-2">Date</th>
                 <th className="border border-gray-400 px-4 py-2">Customer Name</th>
                 <th className="border border-gray-400 px-4 py-2">Collected Amount</th>
