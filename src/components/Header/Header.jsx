9c6aa12194a24dc08c3e81e88066c096fbc5c091
Rohitthorbole <rohitthorbole4@gmail.com>
2025-02-16 20:35:40 +0530

    Header 75 done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,39 +6,35 @@ const Header = () => {
     <>
       <div className='main w-[30vh] h-[100vh] bg-blue-950 m-0 fixed top-0 left-0'>
         <div className='left'>
-          <div className='logo'>
+          <div className='logo pl-2 pt-2'>
             <img src="" className='' alt="" />
-            <p className='text-white'>CloudBill</p>
+            <p className='text-white text-[3vh]'>CloudBill</p>
           </div>
           <div className='features'>
             <ul className='text-white'>
               <li className='flex items-center space-x-3 m-3'>
-                <img src="../src/assets/home.svg" alt="" className='w-4 h-4' />
-                <Link to="/" className='text-white'>Home</Link>
+                <img src="../src/assets/home.svg" alt="" className='w-[1.5vw]' />
+                <Link to="/" className='text-white text-[3vh]'>Home</Link>
               </li>
               <li className='flex items-center space-x-3 m-3'>
-                <img src="../src/assets/sales.svg" alt="" className='w-4 h-4' />
-                <Link to="/Invoice" className='text-white'>Invoice</Link>
+                <img src="../src/assets/sales.svg" alt="" className='w-[1.5vw]' />
+                <Link to="/Invoice" className='text-white text-[3vh]'>Invoice</Link>
               </li>
               <li className='flex items-center space-x-3 m-3'>
-                <img src="../src/assets/customer.svg" alt="" className='w-4 h-4' />
-                <Link to="./Customers" className='text-white'>Customers</Link>
+                <img src="../src/assets/customer.svg" alt="" className='w-[1.5vw]' />
+                <Link to="./Customers" className='text-white text-[3vh]'>Customers</Link>
               </li>
               <li className='flex items-center space-x-3 m-3'>
-                <img src="../src/assets/payments.svg" alt="" className='w-4 h-4' />
-                <Link to="/Inventory" className='text-white'>Inventory</Link>
+                <img src="../src/assets/payments.svg" alt="" className='w-[1.5vw]' />
+                <Link to="/Inventory" className='text-white text-[3vh]'>Inventory</Link>
               </li>
               <li className='flex items-center space-x-3 m-3'>
-                <img src="../src/assets/reports.svg" alt="" className='w-4 h-4' />
-                <Link to="/Sales" className='text-white'>Sales</Link>
+                <img src="../src/assets/reports.svg" alt="" className='w-[1.5vw]' />
+                <Link to="/Sales" className='text-white text-[3vh]'>Sales</Link>
               </li>
-              {/* <li className='flex items-center space-x-3 m-3'>
-                <img src="../src/assets/payments.svg" alt="" className='w-4 h-4' />
-                <Link to="/Payments" className='text-white'>Payments</Link>
-              </li> */}
               <li className='flex items-center space-x-3 m-3'>
-                <img src="../src/assets/about.svg" alt="" className='w-4 h-4' />
-                <Link to="/About" className='text-white'>Account</Link>
+                <img src="../src/assets/about.svg" alt="" className='w-[1.5vw]' />
+                <Link to="/About" className='text-white text-[3vh]'>Account</Link>
               </li>
             </ul>
           </div>
