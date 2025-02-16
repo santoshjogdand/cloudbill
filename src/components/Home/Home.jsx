0f68dd1ff5ad5af42456e1c2ed703dc6e8c6231d
Rohitthorbole <rohitthorbole4@gmail.com>
2025-02-16 20:18:45 +0530

    Home 75 done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,15 +9,15 @@ const Home = () => {
         {/* First Row */}
         <div className='grid grid-cols-3 gap-[200px]'>
           <div className='text-center'>
-            <Link to="/Invoice"><img src="../src/assets/invoice.png" alt="bill" className='w-20' /></Link>
+            <Link to="/Invoice"><img src="../src/assets/invoice.png" alt="bill" className='w-[7vw]' /></Link>
             <Link to="/Invoice"><label className="block mt-2">Invoice</label></Link>
           </div>
           <div className='text-center'>
-            <Link to="/Customers"><img src="../src/assets/rating.png" alt="user" className='w-20' /></Link>
+            <Link to="/Customers"><img src="../src/assets/rating.png" alt="user" className='w-[7vw]' /></Link>
             <Link to="/Customers"><label className="block mt-2">Customers</label></Link>
           </div>
           <div className='text-center'>
-            <Link to="/Sales"><img src="../src/assets/profit.png" alt="sales" className='w-20' /></Link>
+            <Link to="/Sales"><img src="../src/assets/profit.png" alt="sales" className='w-[7vw]' /></Link>
             <Link to="/Sales"><label className="block mt-2">Sales</label></Link>
           </div>
         </div>
@@ -29,11 +29,11 @@ const Home = () => {
             <Link to="/Payments"><label className="block mt-2">Payment</label></Link>
           </div> */}
           <div className='text-center'>
-            <Link to="/Inventory"><img src="../src/assets/inventory.png" alt="inventory" className='w-20' /></Link>
+            <Link to="/Inventory"><img src="../src/assets/inventory.png" alt="inventory" className='w-[7vw]' /></Link>
             <Link to="/Inventory"><label className="block mt-2">Inventory</label></Link>
           </div>
           <div className='text-center'>
-            <Link to="/About"><img src="../src/assets/about.png" alt="information" className='w-20' /></Link>
+            <Link to="/About"><img src="../src/assets/about.png" alt="information" className='w-[7vw]' /></Link>
             <Link to="/About"><label className="block mt-2">Account</label></Link>
           </div>
         </div>
