d3fd13dcda1f73d01202787386edeb56848da6a3
Rohitthorbole <rohitthorbole4@gmail.com>
2025-02-16 20:38:38 +0530

    Home done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,31 +10,27 @@ const Home = () => {
         <div className='grid grid-cols-3 gap-[200px]'>
           <div className='text-center'>
             <Link to="/Invoice"><img src="../src/assets/invoice.png" alt="bill" className='w-[7vw]' /></Link>
-            <Link to="/Invoice"><label className="block mt-2">Invoice</label></Link>
+            <Link to="/Invoice"><label className="block mt-2 text-lg">Invoice</label></Link>
           </div>
           <div className='text-center'>
             <Link to="/Customers"><img src="../src/assets/rating.png" alt="user" className='w-[7vw]' /></Link>
-            <Link to="/Customers"><label className="block mt-2">Customers</label></Link>
+            <Link to="/Customers"><label className="block mt-2 text-lg">Customers</label></Link>
           </div>
           <div className='text-center'>
             <Link to="/Sales"><img src="../src/assets/profit.png" alt="sales" className='w-[7vw]' /></Link>
-            <Link to="/Sales"><label className="block mt-2">Sales</label></Link>
+            <Link to="/Sales"><label className="block mt-2 text-lg">Sales</label></Link>
           </div>
         </div>
 
         {/* Second Row */}
         <div className='grid grid-cols-3 gap-x-[200px]'>
-          {/* <div className='text-center'>
-            <Link to="/Payments"><img src="../src/assets/income.png" alt="payment" className='w-20' /></Link>
-            <Link to="/Payments"><label className="block mt-2">Payment</label></Link>
-          </div> */}
           <div className='text-center'>
             <Link to="/Inventory"><img src="../src/assets/inventory.png" alt="inventory" className='w-[7vw]' /></Link>
-            <Link to="/Inventory"><label className="block mt-2">Inventory</label></Link>
+            <Link to="/Inventory"><label className="block mt-2 text-lg">Inventory</label></Link>
           </div>
           <div className='text-center'>
             <Link to="/About"><img src="../src/assets/about.png" alt="information" className='w-[7vw]' /></Link>
-            <Link to="/About"><label className="block mt-2">Account</label></Link>
+            <Link to="/About"><label className="block mt-2 text-lg">Account</label></Link>
           </div>
         </div>
       </div>
